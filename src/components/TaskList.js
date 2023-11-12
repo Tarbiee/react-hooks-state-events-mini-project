c3e7ede6306acfeb4b4a4973054a2cbdbad7defa
Tarbiee <dorcusstaby@gmail.com>
2023-11-12 15:46:08 +0300

    completed asssignmrnt

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({tasks}) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+    <ul>
+      {tasks.map((task) => (
+      <Task key={task.id} task={task} />  
+      ))}
+    </ul>
     </div>
   );
 }
